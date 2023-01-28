427e8aa77cf274552e1fcd1e6812617cf0b06634
Satyam0204 <satyampattanaik007@gmail.com>
2023-01-29 00:53:35 +0530

    shifted the userinfo in a component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import { MantineProvider } from "@mantine/core"
+import { ApplicationContainer } from "./comps/ApplicationContainer"
 
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{colorScheme:"light"}}>
+      <ApplicationContainer>
+          <Component {...pageProps} />
+      </ApplicationContainer>
+    </MantineProvider>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,81 @@
-import { Table, ScrollArea, Avatar, Text } from '@mantine/core';
-import Form from './comps/form'
+// import { Table, ScrollArea, Avatar, Text } from '@mantine/core';
+// import Form from './comps/form'
 
-export const getStaticProps =async () =>{
-  const res= await fetch('https://63c57732f80fabd877e93ed1.mockapi.io/api/v1/users')
-  const data = await res.json()
+// export const getStaticProps =async () =>{
+//   const res= await fetch('https://63c57732f80fabd877e93ed1.mockapi.io/api/v1/users')
+//   const data = await res.json()
 
-  return {
-      props:{
-          data:data.slice(0,10),
-      }
-  }
-} 
+//   return {
+//       props:{
+//           data:data.slice(0,10),
+//       }
+//   }
+// } 
 
 
 
-export default function UsersRolesTable({ data }) {
+// export default function UsersRolesTable({ data }) {
 
-  const rows = data.map((item) => (
+//   const rows = data.map((item) => (
 
-    <tr key={item.id} >
+//     <tr key={item.id} >
 
-      <td  >
+//       <td  >
 
-          <Avatar size={40} src={item.avatar} radius={40} />
+//           <Avatar size={40} src={item.avatar} radius={40} />
 
       
-      </td>
-      <td>
+//       </td>
+//       <td>
 
-          <div >
+//           <div >
 
-            <Text size="sm"  >
-              {item.email? item.email:item.name}
-            </Text>
-          </div>
-      </td>
+//             <Text size="sm"  >
+//               {item.email? item.email:item.name}
+//             </Text>
+//           </div>
+//       </td>
       
 
 
 
-    </tr>
-  ));
+//     </tr>
+//   ));
 
 
-  return (
-    <>
-    <ScrollArea>
-      <Table sx={{ maxWidth: 300 }} verticalSpacing="md" align='center' >
-        <thead>
-          <tr>
-            <th>Employee</th>
-            <th>Name/Email</th>
+//   return (
+//     <>
+//     <ScrollArea>
+//       <Table sx={{ maxWidth: 300 }} verticalSpacing="md" align='center' >
+//         <thead>
+//           <tr>
+//             <th>Employee</th>
+//             <th>Name/Email</th>
 
-          </tr>
-        </thead>
-        <tbody >{rows}</tbody>
-      </Table>
+//           </tr>
+//         </thead>
+//         <tbody >{rows}</tbody>
+//       </Table>
     
 
-    </ScrollArea>
-    hello
-    <Form />
-    </>
-  );
+//     </ScrollArea>
+//     hello
+//     <Form />
+//     </>
+//   );
 
-  }
+//   }
 
+import UsersRolesTable from './comps/UserRoles'
+
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+
+    </div>
+  )
+}
 
